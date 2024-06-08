264ca434ccb973980b54ca1f6326452658850726
black-tiger4 <3030951264@qq.com>
2024-06-08 17:00:09 +0800

    修改项目基础路径

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/greedy-snake/',
+  base: '/greedy_snake/',
   build: {
     outDir: 'docs',
   },
